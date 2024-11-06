eb294298de7f945a3cf6e75968cf66977fbd6b83
Alex Barker <alex.barker234@gmail.com>
2024-11-06 14:37:34 +0800

    test offline func

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,12 @@ import Link from "next/link";
 export default function Nav() {
   const text = "songversation.";
 
+  let environment = "";
+  if (process.env.NODE_ENV === "development") environment = "local";
+  else if (process.env.ENVIRONMENT === "dev") environment = "dev";
+
   return (
-    <nav className="flex items-center justify-center bg-primary p-1">
+    <nav className="relative flex items-center justify-center bg-primary p-1">
       <Link href="/" className="group p-2 text-3xl font-bold text-black">
         {text.split("").map((char, index) => (
           <span
@@ -17,6 +21,11 @@ export default function Nav() {
           </span>
         ))}
       </Link>
+      {environment && (
+        <span className="pointer-events-none absolute bottom-0 left-1/2 -translate-x-1/2 text-sm font-bold text-black">
+          {environment}
+        </span>
+      )}
     </nav>
   );
 }
